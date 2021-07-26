a7495be8bb70d855a1031216b4506e6550522675
Wafaaabdallah <wafaa.azzam17@gmail.com>
2021-07-26 15:19:49 +0300

    user controller is done

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const userRoutes = require("./routes/userRoute");
+const passport = require("passport");
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
+
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
+app.use(userRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message || "Internal Server Error",
+    });
+  });
+  
+  app.use((req, res, next) => {
+    res.status(404).json({ message: "Path not found" });
+  });
+  
 
 const port = 8000;
 app.listen(8000);
