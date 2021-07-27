19ae00dc7d2361c4a9cbf763eebecffacbd4d650
Wafaaabdallah <wafaa.azzam17@gmail.com>
2021-07-27 11:27:17 +0300

    change message model

--- db/models/Message.js
+++ db/models/Message.js
@@ -1,8 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
     const Message = sequelize.define("Message", {
-      input: {
+      message: {
         type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      timestamp: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      received: {
+        type: DataTypes.BOOLEAN,
         allowNull: false,
       },
          });
     return Message;
+        }
